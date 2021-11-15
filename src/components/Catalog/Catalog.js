93e19bf6782f7a828b3f337113f457188a1fd18b
Nikolay Ivanov <niki_8905@abv.bg>
2021-11-15 14:38:34 +0200

    Added loading title to show until query is done.

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -3,23 +3,35 @@ import CatalogGameCard from './CatalogGameCard';
 
 function Catalog() {
 	const [games, setGames] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
 
 	useEffect(() => {
-		fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
-			.then((res) => res.json())
-			.then((games) => setGames(games));
+		setIsLoading(true);
+		setTimeout(() => {
+			fetch(
+				'http://localhost:3030/data/games?sortBy=_createdOn%20desc'
+			)
+				.then((res) => res.json())
+				.then((games) => {
+					setGames(games);
+					setIsLoading(false);
+				});
+		}, 1000);
 	}, []);
+
+	function getAllGames() {
+		if (games.length === 0) {
+			return <h3 className="no-articles">No articles yet</h3>;
+		}
+		return games.map((game) => (
+			<CatalogGameCard key={game._id} currentGame={game} />
+		));
+	}
 
 	return (
 		<section id="catalog-page">
 			<h1>All Games</h1>
-			{games.length === 0 ? (
-				<h3 className="no-articles">No articles yet</h3>
-			) : (
-				games.map((game) => (
-					<CatalogGameCard key={game._id} currentGame={game} />
-				))
-			)}
+			{isLoading === true ? <h1>loading...</h1> : getAllGames()}
 		</section>
 	);
 }
