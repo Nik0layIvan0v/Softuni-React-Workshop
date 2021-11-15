714160401e20935e0966ae9e78fafe4f01080661
Nikolay Ivanov <niki_8905@abv.bg>
2021-11-15 14:53:20 +0200

    Update Catalog.js

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import CatalogGameCard from './CatalogGameCard';
+import * as gameService from '../../services/gameService';
 
 function Catalog() {
 	const [games, setGames] = useState([]);
