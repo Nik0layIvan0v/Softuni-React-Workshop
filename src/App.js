b3d129d6caec5620c4342d08acf95d04d5ca09d5
Nikolay Ivanov <niki_8905@abv.bg>
2021-11-15 11:58:11 +0200

    router now can render components based on route from navigation bar.

--- src/App.js
+++ src/App.js
@@ -6,30 +6,35 @@ import Header from "./components/Header/Header";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import WelcomeWorld from "./components/WelcomeWorld/WelcomeWorld";
+import { useState } from "react";
 
 function App() {
+  const [page, setPage] = useState("/home");
+
   const routes = {
-    "/home": WelcomeWorld,
-    "/all-games": Catalog,
-    "/create-game": Create,
-    "/login": Login,
-    "/register": Register,
+    "/home": <WelcomeWorld />,
+    "/all-games": <Catalog />,
+    "/create-game": <Create />,
+    "/login": <Login />,
+    "/register": <Register />,
+    "/edit-game": <Edit />,
+    "/game-details": <Details />,
   };
 
   const navigationChangeHandler = (path) => {
-    console.log(`navigationChangeHandler: ${path}`);
+    setPage(path);
   };
 
   return (
     <div id="box">
       <Header navigationChangeHandler={navigationChangeHandler} />
-      <main id="main-content"></main>
-      <Catalog />
+      <main id="main-content">{routes[page] || <h1>No Page Found!</h1>}</main>
+      {/* <Catalog />
       <Create />
       <Details />
       <Edit />
       <Login />
-      <Register />
+      <Register /> */}
     </div>
   );
 }
