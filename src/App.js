d6bf279393779cd53b997d7ec64a3bc4da29d400
Nikolay Ivanov <niki_8905@abv.bg>
2021-11-15 10:55:21 +0200

    Added routes

--- src/App.js
+++ src/App.js
@@ -8,11 +8,22 @@ import Register from "./components/Register/Register";
 import WelcomeWorld from "./components/WelcomeWorld/WelcomeWorld";
 
 function App() {
+  const routes = {
+    "/home": WelcomeWorld,
+    "/all-games": Catalog,
+    "/create-game": Create,
+    "/login": Login,
+    "/register": Register,
+  };
+
+  const navigationChangeHandler = (path) => {
+    console.log(`navigationChangeHandler: ${path}`);
+  };
+
   return (
     <div id="box">
+      <Header navigationChangeHandler={navigationChangeHandler} />
       <main id="main-content"></main>
-      <Header />
-      <WelcomeWorld />
       <Catalog />
       <Create />
       <Details />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,28 @@
-function Header() {
+function Header({ navigationChangeHandler }) {
+  const onHeaderClick = (e) => {
+    e.preventDefault();
+    if (e.target.tagName === "A") {
+      let url = new URL(e.target.href);
+      navigationChangeHandler(url.pathname);
+    }
+  };
+
   return (
-    <header>
+    <header onClick={onHeaderClick}>
       <h1>
-        <a className="home" href="index.html">
+        <a className="home" href="/home">
           GamesPlay
         </a>
       </h1>
       <nav>
-        <a href="index.html">All games</a>
+        <a href="/all-games">All games</a>
         <div id="user">
-          <a href="index.html">Create Game</a>
-          <a href="index.html">Logout</a>
+          <a href="/create-game">Create Game</a>
+          <a href="/logout">Logout</a>
         </div>
         <div id="guest">
-          <a href="index.html">Login</a>
-          <a href="index.html">Register</a>
+          <a href="/login">Login</a>
+          <a href="/register">Register</a>
         </div>
       </nav>
     </header>
